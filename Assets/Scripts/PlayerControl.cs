95c12c716bc12c017faae3c983bbd77d7ae10eed
dendefo <daniek332@gmail.com>
2023-08-23 13:31:27 +0300

    Better jump

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -9,8 +9,10 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] float JumpForce;
     [SerializeField] Rigidbody2D rb;
     [SerializeField] bool isTouchingFloor;
+    TilemapCollider2D tile;
     void FixedUpdate()
     {
+        CalculateJump();
         UserInput();
     }
 
@@ -26,24 +28,42 @@ public class PlayerControl : MonoBehaviour
         rb.AddForce(Vector3.up* JumpForce*Time.fixedDeltaTime,ForceMode2D.Impulse);
         
     }
+    private void CalculateJump()
+    {
+        if (rb.velocity.y == 0) isTouchingFloor = true;
+        else isTouchingFloor = false;
+    }
     private void Move(bool isRight)
     {
         transform.Translate(MoveForce * Vector3.right * (isRight ? 1 : -1) * Time.fixedDeltaTime);
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "TileMap")
-        {
-            
-            if (collision.contacts[collision.contactCount-1].point.y<(transform.position.y-transform.localScale.y)) isTouchingFloor = true;
-        }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "TileMap")
-        {
-            if (collision.contactCount == 0) isTouchingFloor = false;
-        }
-    }
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "TileMap")
+    //    {
+    //        int legitContacts = 0;
+    //        foreach (var contact in collision.contacts)
+    //        {
+    //            //Debug.Log(((Vector2)transform.position - contact.point).y);
+    //            if (((Vector2)transform.position - contact.point).y >= transform.localScale.y) legitContacts++;
+    //        }
+    //        Debug.Log(legitContacts);
+    //        if (legitContacts == 0) isTouchingFloor = true;
+    //        else isTouchingFloor = false;
+
+    //    }
+    //}
+    //private void OnCollisionStay2D(Collision2D collision)
+    //{
+    //    OnCollisionEnter2D(collision);
+
+    //}
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "TileMap")
+    //    {
+    //        //if (collision.contactCount == 0) isTouchingFloor = false;
+    //    }
+    //}
     
 }
