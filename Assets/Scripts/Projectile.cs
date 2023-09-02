34209e0c05aca8dc16f3e239e458e5f63318f6e9
dendefo <daniek332@gmail.com>
2023-09-03 00:35:36 +0300

    small changes

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -11,13 +11,18 @@ public class Projectile : MonoBehaviour
     {
         rb.velocity = velocity;
     }
-    public void Shoot(Vector3 position,float speed,float damage)
+    public void Shoot(Vector3 position, float speed, float damage)
     {
-        velocity = (Vector2)(Vector3.Normalize((position-transform.position))*speed);
+        velocity = (Vector2)(Vector3.Normalize((position - transform.position)) * speed);
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    virtual public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Player") return;
+        else Destroy(gameObject);
+    }
+    virtual public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player") return;
         else Destroy(gameObject);
     }
 }
